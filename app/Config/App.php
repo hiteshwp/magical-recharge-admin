4ac971887558bb9fa788acac7fa3f6d8e6bf7f39
Hitesh Prajapati <hiteshvpra@gmail.com>
2024-05-21 16:00:14 +0530

    Just commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://stagingurlhub.in/projects/magical-recharge-admin/';
+    public string $baseURL = 'http://localhost/hitesh/magical-recharge-admin/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
