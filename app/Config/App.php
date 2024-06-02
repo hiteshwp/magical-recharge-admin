d4fa6e01fc6897d44084e4f4f2c46c8fbb8f3c48
Hitesh Prajapati <hiteshvpra@gmail.com>
2024-06-02 18:56:03 +0530

    Update api as per the new requirements

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,8 +17,8 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
 
-    #public string $baseURL = 'http://localhost/hitesh/ci/magical-recharge-admin/';
-    public string $baseURL = 'https://stagingurlhub.in/projects/magical-recharge-admin/';
+    public string $baseURL = 'http://localhost/hitesh/ci/magical-recharge-admin/';
+    #public string $baseURL = 'https://stagingurlhub.in/projects/magical-recharge-admin/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
