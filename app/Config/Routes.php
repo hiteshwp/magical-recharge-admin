a2497dc40e812d8c6ba2de66feb0afe35b5d8b22
Hitesh Prajapati <hiteshvpra@gmail.com>
2024-06-04 15:24:34 +0530

    Implement new password api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -45,4 +45,5 @@ $routes->group("api", ["namespace"=> "App\Controllers\Api"], function ($routes) 
     $routes->post('user-registration', 'ApiController::user_registration');
     $routes->post('user-login', 'ApiController::user_login');
     $routes->post('forgot-password', 'ApiController::forgot_password');
+    $routes->post('new-password', 'ApiController::new_password');
 });
